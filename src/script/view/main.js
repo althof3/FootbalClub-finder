9905185a3b7f76c4e272c056e0a8c22f43bcb0a0
althof <althoframdhan3@gmail.com>
2020-07-24 20:22:35 +0700

    add files

---
+++ src/script/view/main.js
@@ -0,0 +1,26 @@
+import DataSource from '../data/data-source.js';
+import '../component/search-bar.js';
+import '../component/club-list.js';
+
+const main = ()  => {
+    const searchElement = document.querySelector("search-bar");
+    const clubListElement = document.querySelector("club-list");
+
+    const onButtonSearchClicked = () => {
+        DataSource.searchClub(searchElement.value)
+        .then(renderResult)
+        .catch(fallbackResult)
+    };
+
+    const renderResult = results => {
+        clubListElement.club = results;
+    };
+
+    const fallbackResult = message => {
+        clubListElement.renderError(message);
+    };
+
+    searchElement.clickEvent = onButtonSearchClicked;
+};
+
+export default main;
